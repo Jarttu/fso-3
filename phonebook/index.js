0a6e174238a8d8401101e923eb331cf3f8740626
Arttu Jääskeläinen <jaahas.ihminen@gmail.com>
2026-03-05 09:25:07 +0200

    tehtävä 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,22 +3,22 @@ const app = express();
 
 let persons = [
     {
-        "id": "1",
+        "id": 1,
         "name": "Arto Hellas",
         "number": "040-123456"
     },
     {
-        "id": "2",
+        "id": 2,
         "name": "Ada Lovelace",
         "number": "39-44-5323523"
     },
     {
-        "id": "3",
+        "id": 3,
         "name": "Dan Abramov",
         "number": "12-43-234345"
     },
     {
-        "id": "4",
+        "id": 4,
         "name": "Mary Poppendieck",
         "number": "39-23-6423123"
     }
@@ -27,6 +27,16 @@ let persons = [
 app.get('/api/persons', (request, response) => {
     response.json(persons);
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
 
 app.get('/info', (request, response) => {
     const personsCount = persons.length;
