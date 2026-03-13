ec2d859de4b3ff5b8e48ef42c957727c234253af
Arttu Jääskeläinen <jaahas.ihminen@gmail.com>
2026-03-13 11:35:34 +0200

    3.4

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,30 +3,62 @@ const app = express();
 
 let persons = [
     {
-        "id": "1",
+        "id": 1,
         "name": "Arto Hellas",
         "number": "040-123456"
     },
     {
-        "id": "2",
+        "id": 2,
         "name": "Ada Lovelace",
         "number": "39-44-5323523"
     },
     {
-        "id": "3",
+        "id": 3,
         "name": "Dan Abramov",
         "number": "12-43-234345"
     },
     {
-        "id": "4",
+        "id": 4,
         "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        "number": "39-23-6423123"
     }
 ]
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.get('/info', (request, response) => {
+    const personsCount = persons.length;
+    const date = new Date();
+
+    response.send(`
+        <p>Phonebook has info for ${personsCount} people</p>
+        <p>${date}</p>
+    `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const personExists = persons.some(p => p.id === id);
+
+    if (personExists) {
+        persons = persons.filter(p => p.id !== id);
+        response.status(204).end();
+    } else {
+        response.status(404).end();
+    }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
