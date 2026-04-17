210314801fc6c13a74c6d852261e3055eb3f4b80
Jarttu <Jaahas.ihminen@gmail.com>
2026-04-17 10:51:59 +0300

    3.22 eslint for backend and frontend

---
+++ phonebook/eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  	{ignores: ["**/dist/**","**/build/**","**/node_modules/**"]},
+  
+  	{files: ["**/*.{js,mjs,cjs}"],plugins: { js },extends: ["js/recommended"],languageOptions: {globals: globals.node}},
+
+  	{files: ["**/*.js"],languageOptions: {sourceType: "commonjs"}}
+]);
--- react-project/eslint.config.js
+++ react-project/eslint.config.js
@@ -1,29 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: ["**/dist/**", "**/build/**", "**/node_modules/**"]
+  },
+
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    plugins: {
+      js,
+      react: pluginReact
+    },
     extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      "js/recommended",
+      pluginReact.configs.flat.recommended
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      globals: globals.browser
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
+    }
+  }
+]);
